7d54875c554c823f2bb41b3533175d1215552d74
iyuksel78 <97805177+iyuksel78@users.noreply.github.com>
2022-04-22 21:47:26 +0300

    upload form remote

--- src/LoginTest_Ismail.java
+++ src/LoginTest_Ismail.java
@@ -3,6 +3,6 @@ public class LoginTest_Ismail {
         System.out.println("Initial Change and first class is created");
         System.out.println("AC1 testing with valid credentials is passed");
         System.out.println("AC2 testing with valid credentials is passed");
-        System.out.println("AC3 testing");
+        System.out.println("first change from remote");
     }
 }
