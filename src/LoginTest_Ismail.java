54f0e83b847aaf208ee24f70b0778932bf06fdea
iyuksel78 <97805177+iyuksel78@users.noreply.github.com>
2022-04-22 21:34:42 +0300

    AC2 is completed

--- src/LoginTest_Ismail.java
+++ src/LoginTest_Ismail.java
@@ -2,5 +2,6 @@ public class LoginTest_Ismail {
     public static void main(String[] args) {
         System.out.println("Initial Change and first class is created");
         System.out.println("AC1 testing with valid credentials is passed");
+        System.out.println("AC2 testing with valid credentials is passed");
     }
 }
